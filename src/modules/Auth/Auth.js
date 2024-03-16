37f76ea06b64cc36d18b4b76f0c69497b328a77e
Kasinathan <way2kasinathb35@gmail.com>
2024-03-16 19:36:54 +0530

    auth js started

--- src/modules/Auth/Auth.js
+++ src/modules/Auth/Auth.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import "./auth.css";
+
 
 function Auth() {
   return (
     <div>
         <Navbar/>
+        <div className='auth_container'>
+          <div className='heading'>
+            <h1>Login to Spotify</h1>
+          </div>
+        </div>
     </div>
   )
 }
