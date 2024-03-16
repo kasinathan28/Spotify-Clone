8950172707eef1c7a4e2b1018b0965baca4812be
kasinathan28 <way2kasinathb35@gmail.com>
2024-03-16 20:42:41 +0530

    Login form on pregress

--- src/modules/Auth/Auth.js
+++ src/modules/Auth/Auth.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import "./auth.css";
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import './auth.css';
 
+// Images
+import google from "./assets/google.png";
+import Facebook from "./assets/facebook.png";
+import Phone from "./assets/phone.png";
 
 function Auth() {
   return (
-    <div>
-        <Navbar/>
-        <div className='auth_container'>
-          <div className='heading'>
-            <h1>Login to Spotify</h1>
+    <div className='authmain'>
+      <Navbar />
+      <div className='auth_container'>
+        <div className='heading'>
+          <h1>Log in to Spotify</h1>
+        </div>
+        <div className='cta'>
+          <button className='google_btn'> <img src={google} alt='google'/> Continue With Google</button>
+          <button className='facebook_btn'> <img src={Facebook} alt='facebook'/>  Continue With Facebook</button>
+          <button className='phone_btn'> <img src={Phone} alt='Phone' id='phone'/>  Continue With Phone</button>
+        </div>
+        <div className='login_form_container'>
+          <div className='login_form'>
+          <label> Email or phone</label>
+          <input type='text'  placeholder='Email or Phone'/>
+          <label> Email or phone</label>
+          <input type='password' placeholder='Password'/>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
