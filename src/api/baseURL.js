92884ae34c0dae0ad036f62785bb58617bb25173
Kasinathan <way2kasinathb35@gmail.com>
2024-03-17 21:17:38 +0530

    base url

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,1 +1,1 @@
-export const BASEURL = 'http://localhost:5000'
+export const BASEURL = 'https://spotify-server-dcbn.onrender.com'
--- src/modules/Auth/Auth.js
+++ src/modules/Auth/Auth.js
@@ -14,7 +14,6 @@ function Auth() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  
   const handleUserLogin = async () => {
     try {
       const response = await Login(username, password);
