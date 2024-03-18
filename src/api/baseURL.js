e12cec0f298163f3c76699ec68e75da80a6457e2
Kasinathan <way2kasinathb35@gmail.com>
2024-03-18 12:11:23 +0530

    Base url updated

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,2 +1,2 @@
-export const BASEURL = 'http://localhost:5000'
-// export const BASEURL = 'https://spotify-server-dcbn.onrender.com'
+// export const BASEURL = 'http://localhost:5000'
+export const BASEURL = 'https://spotify-server-dcbn.onrender.com'
