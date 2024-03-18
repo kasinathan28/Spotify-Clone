88892ee26fde8277baebce67d9fc8d7cf65832e7
Kasinathan <way2kasinathb35@gmail.com>
2024-03-18 11:35:21 +0530

    Loading and warning for input fields

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./loader.css";
+
+
+function Loader() {
+  return (
+    <div>
+      <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Loader
--- src/modules/Auth/Auth.js
+++ src/modules/Auth/Auth.js
@@ -4,17 +4,28 @@ import "./auth.css";
 import { Switch } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { Login } from "../../api/AuthAPI";
-import googleIcon from "./assets/google.png";
+import Loader from "../../components/loader/Loader";
+// import googleIcon from "./assets/google.png";
 import Facebook from "./assets/facebook.png";
 import Phone from "./assets/phone.png";
 import { GoogleLoginButton } from "react-social-login-buttons";
 import { LoginSocialGoogle } from "reactjs-social-login";
+
 function Auth() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
 
   const handleUserLogin = async () => {
+    if (!username || !password) {
+      setShowWarning(true);
+      setTimeout(() => setShowWarning(false), 2000); // Hide warning after 3 seconds
+      return;
+    }
+    
+    setLoading(true);
     try {
       const response = await Login(username, password);
       console.log(response.message);
@@ -25,6 +36,8 @@ function Auth() {
       }
     } catch (error) {
       console.error("Error during login:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -38,61 +51,80 @@ function Auth() {
     // Handle Google login failure
   };
 
+
   return (
-      <div className="authmain">
-        <Navbar />
-        <div className="auth_container">
-          <div className="heading">
-            <h1>Log in to Spotify</h1>
-          </div>
-          <div className="cta">
+    <div className="authmain">
+      <Navbar />
+      <div className="auth_container">
+        <div className="heading">
+          <h1>Log in to Spotify</h1>
+        </div>
+        <div className="cta">
           <LoginSocialGoogle
-        client_id={"511565921941-ug4fmmbmoag00u302p2p3cgmdgvur7ef.apps.googleusercontent.com"}
-        scope="openid profile email"
-        discoveryDocs="claims_supported"
-        access_type="offline"
-        onResolve={handleGoogleLoginSuccess}
-        onReject={handleGoogleLoginFailure}
-      >
-        <GoogleLoginButton />
-      </LoginSocialGoogle>
-            <button className="facebook_btn">
-              <img src={Facebook} alt="facebook" /> Continue With Facebook
-            </button>
-            <button className="phone_btn">
-              <img src={Phone} alt="Phone" id="phone" /> Continue With Phone
-            </button>
+            client_id={"511565921941-ug4fmmbmoag00u302p2p3cgmdgvur7ef.apps.googleusercontent.com"}
+            scope="openid profile email"
+            discoveryDocs="claims_supported"
+            access_type="offline"
+            onResolve={handleGoogleLoginSuccess}
+            onReject={handleGoogleLoginFailure}
+          >
+            <GoogleLoginButton />
+          </LoginSocialGoogle>
+          <button className="facebook_btn">
+            <img src={Facebook} alt="facebook" /> Continue With Facebook
+          </button>
+          <button className="phone_btn">
+            <img src={Phone} alt="Phone" id="phone" /> Continue With Phone
+          </button>
+        </div>
+
+
+
+        <div className="login_form_container">
+          <div className="login_form">
+            <label>Email or Username</label>
+            <input
+              type="text"
+              placeholder="Email or Phone"
+              value={username}
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div className="login_form_container">
-            <div className="login_form">
-              <label>Email or Username</label>
-              <input type="text" placeholder="Email or Phone" value={username} onChange={(e) => setUsername(e.target.value)} />
-              <label>Password</label>
-              <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="remember_me">
-              <Switch
-                id="remember_me_checkbox"
-                sx={{
-                  "& .MuiSwitch-track": {
-                    backgroundColor: "#1ed760",
-                    opacity: 0.7,
-                  },
-                  "& .Mui-checked": {
-                    color: "#1ed760",
-                  },
-                }}
-              />
-              <label htmlFor="remember_me_checkbox">Remember Me</label>
-            </div>
-            <button onClick={handleUserLogin}>Log in</button>
-            <p>Forgot your password?</p>
+          <div className="remember_me">
+            <Switch
+              id="remember_me_checkbox"
+              sx={{
+                "& .MuiSwitch-track": {
+                  backgroundColor: "#1ed760",
+                  opacity: 0.7,
+                },
+                "& .Mui-checked": {
+                  color: "#1ed760",
+                },
+              }}
+            />
+            <label htmlFor="remember_me_checkbox">Remember Me</label>
           </div>
-          <div className="lastdiv">
-            <h3>Don't have an account? <span>Sign up</span> </h3>
-          </div>
+          {showWarning && <p style={{ color: "red", textDecoration: "none" }}>Please fill in all fields</p>}
+          <button onClick={handleUserLogin}>
+            {loading ? <Loader /> : "Log in"}
+          </button>
+          <p>Forgot your password?</p>
+        </div>
+        <div className="lastdiv">
+          <h3>Don't have an account? <span>Sign up</span> </h3>
         </div>
       </div>
+    </div>
   );
 }
 
